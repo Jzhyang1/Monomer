cc93b6510d7a96853bd2ce5880de92f4d9865353
aidan <aidan.zhlai@gmail.com>
2023-07-26 16:30:33 -0500

    LiteralNode and its basic subclasses

--- src/main/java/systems/merl/monomer/syntaxTree/LiteralNode.java
+++ src/main/java/systems/merl/monomer/syntaxTree/LiteralNode.java
@@ -1,4 +1,16 @@
 package systems.merl.monomer.syntaxTree;
 
 public abstract class LiteralNode extends Node {
+
+    public Usage usage;
+
+    public LiteralNode(String name) {
+        super(name);
+        usage = Usage.LITERAL;
+    }
+
+    public Usage getUsage() {
+        return usage;
+    }
+
 }
--- src/main/java/systems/merl/monomer/syntaxTree/StringNode.java
+++ src/main/java/systems/merl/monomer/syntaxTree/StringNode.java
@@ -1,4 +1,34 @@
 package systems.merl.monomer.syntaxTree;
 
+import systems.merl.monomer.compiler.CompileSize;
+import systems.merl.monomer.compiler.CompileValue;
+import systems.merl.monomer.interpreter.InterpretBaseValue;
+import systems.merl.monomer.interpreter.InterpretValue;
+
 public class StringNode extends LiteralNode{
+
+    private String value;
+
+
+    public StringNode(String s) {
+        super("\"" + s + "\"");
+        value = s;
+    }
+
+    @Override
+    public InterpretValue interpretValue() {
+        return new InterpretBaseValue<>(value);
+    }
+
+    @Override
+    public CompileValue compileValue() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public CompileSize compileSize() {
+        //TODO
+        return null;
+    }
 }
