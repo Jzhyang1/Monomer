c53a191b20710b838498360313357d08e92c98f1
jzhyang1 <jzhyang1@gmail.com>
2023-07-29 08:49:52 -0500

    Fixed routing

--- web/src/index.js
+++ web/src/index.js
@@ -14,21 +14,32 @@ import ErrorPage from "./pages/ErrorPage";
 import PageWrapper from "./custom_components/PageWrapper";
 import InstallPage from "./pages/InstallPage";
 import DocumentationPage from "./pages/DocumentationPage";
-import OperatorsPage from "./pages/documentation/OperatorsPage";
+import OperatorsPage, { operators } from "./pages/documentation/OperatorsPage";
 import DocumentationHomePage from "./pages/documentation/DocumentationPage";
+import OperatorDetails from "./custom_components/OperatorDetails";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <PageWrapper path="%PUBLIC_URL%">
+    <Router path="%PUBLIC_URL%">
+      <PageWrapper>
         <Routes>
           <Route errorElement element={<ErrorPage />} />
           <Route exact path="/" element={<HomePage />} />
           <Route exact path="/contact" element={<ContactPage />} />
           <Route exact path="/docs" element={<DocumentationPage />}>
+            <Route path="" element={<DocumentationHomePage />} />
             <Route path="*" element={<DocumentationHomePage />} />
-            <Route path="operators" element={<OperatorsPage />} />
+            <Route path="operators">
+              <Route path="" element={<OperatorsPage />} />
+              <Route path="*" element={<OperatorsPage />} />
+              {operators.map((op, i) => (
+                <Route
+                  path={op.name}
+                  element={<OperatorDetails {...op} key={i} />}
+                />
+              ))}
+            </Route>
           </Route>
           <Route exact path="/start" element={<StartPage />} />
           <Route exact path="/install" element={<InstallPage />} />
