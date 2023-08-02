a67ee883a677582c123cfcf521e183af497070f0
jzhyang1 <jzhyang1@gmail.com>
2023-08-02 07:32:04 -0500

    removed web

--- web/src/components/Title.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export default function Title(props) {
-  // eslint-disable-next-line jsx-a11y/heading-has-content
-  return <h1 {...props} className={`text-[50px] ${props.className}`} />;
-}
