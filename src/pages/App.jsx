9431ab994c92a5a40521b468bd0f803ad34c77be
jzhyang1 <jzhyang1@gmail.com>
2023-08-08 09:14:55 -0500

    added context

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./subpages/HomePage";
 import StartPage from "./subpages/StartPage";
@@ -18,57 +18,71 @@ import VariablesPage from "./documentation/VariablesPage";
 import CommandLinePage from "./documentation/CommandLinePage";
 import NavMenu from "../custom_components/NavMenu";
 import Footer from "../custom_components/Footer";
+import { useState } from "react";
+import { ThemeContext } from "../contexts";
 
 export default function App() {
+  const [isDarkMode, setDarkMode] = useState(false);
+  const contextValue = useMemo(
+    () => ({ isDarkMode, setDarkMode }),
+    [isDarkMode]
+  );
+
   return (
     <React.StrictMode>
-      <NavMenu />
-      <div className="relative flex flex-col p-[20px] md:p-[40px] overflow-y-scroll overflow-x-clip">
-        <Routes>
-          <Route path="*" element={<ErrorPage />} />
-          <Route exact path="/" element={<HomePage />} />
-          <Route exact path="/contact" element={<ContactPage />} />
-          <Route exact path="/docs" element={<DocumentationPage />}>
-            <Route path="" element={<DocumentationHomePage />} />
-            <Route path="*" element={<DocumentationHomePage />} />
-            <Route path="operators">
-              <Route path="" element={<OperatorsPage />} />
-              <Route path="*" element={<OperatorsPage />} />
-              {operators.map((op, i) => (
-                <Route
-                  path={op.name}
-                  key={i}
-                  element={<OperatorDetails {...op} />}
-                />
-              ))}
+      <ThemeContext.Provider value={contextValue}>
+        <NavMenu />
+        <div className="relative flex flex-col p-[20px] md:p-[40px] overflow-y-scroll overflow-x-clip">
+          <Routes>
+            <Route path="*" element={<ErrorPage />} />
+            <Route exact path="/" element={<HomePage />} />
+            <Route exact path="/contact" element={<ContactPage />} />
+            <Route exact path="/docs" element={<DocumentationPage />}>
+              <Route path="" element={<DocumentationHomePage />} />
+              <Route path="*" element={<DocumentationHomePage />} />
+              <Route path="operators">
+                <Route path="" element={<OperatorsPage />} />
+                <Route path="*" element={<OperatorsPage />} />
+                {operators.map((op, i) => (
+                  <Route
+                    path={op.name}
+                    key={i}
+                    element={<OperatorDetails {...op} />}
+                  />
+                ))}
+              </Route>
+              <Route path="types">
+                <Route path="" element={<TypesPage />} />
+                <Route path="*" element={<TypesPage />} />
+                {types.map((t, i) => (
+                  <Route
+                    path={t.name}
+                    key={i}
+                    element={<TypeDetails {...t} />}
+                  />
+                ))}
+              </Route>
+              <Route path="controls">
+                <Route path="" element={<ControlsPage />} />
+                <Route path="*" element={<ControlsPage />} />
+                {controls.map((t, i) => (
+                  <Route
+                    path={t.name}
+                    key={i}
+                    element={<ControlDetails {...t} />}
+                  />
+                ))}
+              </Route>
+              <Route path="functions" element={<FunctionsPage />} />
+              <Route path="variables" element={<VariablesPage />} />
+              <Route path="cli" element={<CommandLinePage />} />
             </Route>
-            <Route path="types">
-              <Route path="" element={<TypesPage />} />
-              <Route path="*" element={<TypesPage />} />
-              {types.map((t, i) => (
-                <Route path={t.name} key={i} element={<TypeDetails {...t} />} />
-              ))}
-            </Route>
-            <Route path="controls">
-              <Route path="" element={<ControlsPage />} />
-              <Route path="*" element={<ControlsPage />} />
-              {controls.map((t, i) => (
-                <Route
-                  path={t.name}
-                  key={i}
-                  element={<ControlDetails {...t} />}
-                />
-              ))}
-            </Route>
-            <Route path="functions" element={<FunctionsPage />} />
-            <Route path="variables" element={<VariablesPage />} />
-            <Route path="cli" element={<CommandLinePage />} />
-          </Route>
-          <Route exact path="/start" element={<StartPage />} />
-          <Route exact path="/install" element={<InstallPage />} />
-        </Routes>
-        <Footer />
-      </div>
+            <Route exact path="/start" element={<StartPage />} />
+            <Route exact path="/install" element={<InstallPage />} />
+          </Routes>
+          <Footer />
+        </div>
+      </ThemeContext.Provider>
     </React.StrictMode>
   );
 }
